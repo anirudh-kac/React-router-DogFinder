f077d31fb8cd35189e3da513b035a044df1da994
Anirudh Kachroo <42580581+anirudh-kac@users.noreply.github.com>
2020-11-23 20:16:32 +0530

    Fix age

--- src/Dog.js
+++ src/Dog.js
@@ -7,8 +7,8 @@ export default class Dog extends Component {
                 <div className="card" style={{width: "18rem"}}>
                 <img class="card-img-top" src= {this.props.dog.src} alt= {this.props.dog.name} />
                 <div class="card-body">
-                    <h5 class="card-title">{this.props.dog.name + "years old"}</h5>
-                    <p class="card-text">{this.props.dog.age}</p>
+                    <h5 class="card-title">{this.props.dog.name }</h5>
+                    <p class="card-text">{this.props.dog.age+ "years old" }</p>
                 </div>
                 <ul class="list-group list-group-flush">
                     {
